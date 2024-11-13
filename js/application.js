3227b00dd3f679a1d508ace8015540742076a9d6
ZhouMoon <951176226@qq.com>
2024-11-13 17:19:44 +0800

    Site updated: 2024-11-13 17:19:44

--- js/application.js
+++ js/application.js
@@ -75,3 +75,15 @@ $(function() {
     $menuList[activeIndex] && $($menuList[activeIndex]).addClass('active');
   }
 });
+
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+      window.onload = func;
+  } else {
+      window.onload = function() {
+          oldonload();
+          func();
+      }
+  }
+}
